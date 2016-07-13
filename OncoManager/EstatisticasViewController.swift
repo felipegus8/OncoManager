7e137b89a3135ce7fc3d2b0c0f91f9d1ef59d397
Gabrielle Brandenburg dos Anjos <gabriellebrandenburg@gmail.com>
2016-07-13 16:05:54 -0300

    add class files login, estatisticas, paciente e pacientes

---
+++ OncoManager/EstatisticasViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EstatisticasViewController.swift
+//  OncoManager
+//
+//  Created by Gabrielle Brandenburg dos Anjos on 7/13/16.
+//  Copyright © 2016 Felipe Viberti. All rights reserved.
+//
+
+import UIKit
+
+class EstatisticasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
