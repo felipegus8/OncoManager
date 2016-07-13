e8dcbd13071a8c9f247c32d2259befd6d899d00a
Felipe Viberti <felipegus8@yahoo.com.br>
2016-07-13 10:55:28 -0300

    Mais classes

---
+++ OncoManager/Medico.swift
@@ -0,0 +1,25 @@
+//
+//  Medico.swift
+//  OncoManager
+//
+//  Created by Felipe Viberti on 7/13/16.
+//  Copyright © 2016 Felipe Viberti. All rights reserved.
+//
+
+import Foundation
+
+public class Medico
+{
+    var nome:String!
+    var email: String!
+    var crm: Int!
+    var telefone: Int?
+    
+    init(nome: String, email: String, crm: Int,telefone:Int?) {
+        
+        self.nome = nome
+        self.email = email
+        self.crm = crm
+        self.telefone = telefone
+    }
+}
--- OncoManager/Paciente.swift
+++ OncoManager/Paciente.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+public class Paciente
+{
+    var cpf:Int!
+    var nome:String!
+    var sobrenome:String!
+    var email:String!
+    var RG:String!
+    var telefoneFixo:Int!
+    var celular:Int!
+    var peso:Double!
+    var altura:Double!
+    var alergia:Bool!
+    var tipoAlergia:String?
+    var deficiente:Bool!
+    var tipoDeficiencia:String?
+    var metal:Bool!
+    
+
+    init(cpf:Int,nome:String,sobrenome:String,email:String,RG:String,telefoneFixo:Int,celular:Int,peso:Double,altura:Double,alergia:Bool,tipoAlergia:String?,deficiente:Bool,tipoDeficiencia:String?,metal:Bool)
+    {
+        self.cpf = cpf
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.email = email
+        self.RG = RG
+        self.telefoneFixo = telefoneFixo
+        self.celular = celular
+        self.peso = peso
+        self.altura = altura
+        self.alergia = alergia
+        self.tipoAlergia = tipoAlergia
+        self.deficiente = deficiente
+        self.tipoDeficiencia = tipoDeficiencia
+        self.metal = metal
+    }
+}
