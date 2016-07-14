8f16312d48d78e67da3ba30254dd4ad8253a5adf
Felipe Viberti <felipegus8@yahoo.com.br>
2016-07-14 10:12:41 -0300

    Só pra evitar o conflito

--- OncoManager/Medico.swift
+++ OncoManager/Medico.swift
@@ -7,6 +7,11 @@
 //
 
 import Foundation
+// DaoCloudKit().addAdmin(Admin(email: "Teste", senha: "qualquer", nome: "Teste"))
+let testeExame =  Exame(codigo: 8, nome: "TesteExame", medico: "Testando", local: "Copa", data: "23-08-2016", hora: "17:00", aprovado: 0, realizado: 1)
+let testePaciente = Paciente(cpf: 16798089766, nome: "Teste", bairro: "Magé", bairroPrefere: "Niteroi", dataNasc: "26-09-1962", email: "qualquer", telefoneFixo: 2345980135, celular: 346725789, peso: 78, altura: 1.70, alergia: 0, marcapasso: 0, clipesCirurgico: 0, operado: 0, tipoOperacao: nil, cadeirante: 0, diabetico: 1, hipertenso: 0, convenio: "Assim", tipoPlano: "Ruim", matriculaPlano: "12345",claustrofobico:0)
+//DaoCloudKit().addPaciente(testePaciente)
+//DaoCloudKit().addExameToPaciente(testeExame, paciente: testePaciente)
 
 public class Medico
 {
