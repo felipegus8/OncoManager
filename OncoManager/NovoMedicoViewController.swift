b174c5c209da8b8ba70a9d59dc604c44a6a8618c
Gabrielle Brandenburg dos Anjos <gabriellebrandenburg@gmail.com>
2016-07-21 13:49:40 -0300

    classe da view controller de Novo Médico criada

--- OncoManager/NovoMedicoViewController.swift
+++ OncoManager/NovoMedicoViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class NovoMedicoViewController: UIViewController {
+class NovoMedicoViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nome: OMTextField!
     @IBOutlet weak var email: OMTextField!
     @IBOutlet weak var telefone: OMTextField!
     @IBOutlet weak var especialidade: OMTextField!
     @IBOutlet weak var crm: OMTextField!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,18 +29,46 @@ class NovoMedicoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func linkDelegate(){
+        nome.delegate = self
+        email.delegate = self
+        telefone.delegate = self
+        especialidade.delegate = self
+        crm.delegate = self
     }
-    */
-    @IBAction func cadastrarPressed(sender: UIButton) {
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        //textField.resign
+        self.view.endEditing(true)
+        return true
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
+        let touch = touches.first!
+        
+//        let vieW = touch.view
+//        print(vieW!.tag)
+//        //finaliza a edição quando o usuario toca fora da scrollView
+//        if(vieW?.tag != 8){
+        
+            scrollView.endEditing(true)
+            view.endEditing(true)
+    }
+    
+    @IBAction func stopEditing(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+
+    
+    @IBAction func closeModalPressed(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //MARK: armazenar dados no iCloud
+    @IBAction func cadastrarPressed(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
